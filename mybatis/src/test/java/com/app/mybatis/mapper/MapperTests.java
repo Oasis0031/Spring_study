3119defcf0620f7383977736e1bce62136b825df
Oasis0031 <oasis0031@gmail.com>
2026-04-02 14:06:02 +0900

    feat: spring day02

--- mybatis/src/test/java/com/app/mybatis/mapper/MapperTests.java
+++ mybatis/src/test/java/com/app/mybatis/mapper/MapperTests.java
@@ -1,5 +1,6 @@
 package com.app.mybatis.mapper;
 
+import com.app.mybatis.domain.vo.MemberVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,19 +17,54 @@ public class MapperTests {
     private MemberMapper memberMapper;
 
     @Test
-    public void MapperTest(){
+    public void mapperTests() {
         log.info(timeMapper.getTime());
     }
 
     @Test
-    public void MapperTest2(){
+    public void mapperTests2() {
         log.info(timeMapper.getTime2());
     }
 
     @Test
-    public void MapperTest3(){
+    public void mapperTests3() {
         log.info("{}", memberMapper.selectAll());
-
     }
 
+    @Test
+    public void mapperTests4() {
+        memberMapper.select(21L).map(MemberVO::toString).ifPresent(log::info);
+    }
+
+    @Test
+    public void insertTest(){
+        MemberVO memberVO = new MemberVO();
+//        memberVO.setMemberEmail("test789@gmail.com");
+//        memberVO.setMemberPassword("test123!@#");
+//        memberVO.setMemberName("장보고");
+//
+        memberVO.setMemberEmail("test456@gmail.com");
+        memberVO.setMemberPassword("test123!@#");
+        memberVO.setMemberName("이순신");
+
+        memberMapper.insert(memberVO);
+    }
+
+    @Test
+    public void updateTest(){
+        MemberVO memberVO = new MemberVO();
+        memberVO.setId(21L);
+        memberVO.setMemberEmail("test456@gmail.com");
+        memberVO.setMemberPassword("test123!@#");
+        memberVO.setMemberName("장길동");
+
+        memberMapper.select(21L).map(MemberVO::toString).ifPresent(log::info);
+        memberMapper.update(memberVO);
+        memberMapper.select(21L).map(MemberVO::toString).ifPresent(log::info);
+    }
+
+    @Test
+    public void deleteTest(){
+        memberMapper.delete(21L);
+    }
 }
